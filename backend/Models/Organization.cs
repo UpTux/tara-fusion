8a6f66cf1d52f1d2dec654c4652e9cd4b696c8c9
Patrick Dahlke <patrick.dahlke@pichu.io>
2025-11-10 08:57:46 +0100

    Remove backend files

--- backend/Models/Organization.cs
+++
@@ -1,9 +0,0 @@
-namespace TaraFusion.Models;
-
-public class Organization
-{
-    public Guid Id { get; set; }
-    public required string Name { get; set; }
-    public ICollection<User> Users { get; set; } = new List<User>();
-    public ICollection<Project> Projects { get; set; } = new List<Project>();
-}
