c9a54fadaf97ee75b2b6f76415973974f0eb3eef
Patrick Dahlke <patrick.dahlke@pichu.io>
2025-11-11 14:36:27 +0100

    Test report + coverage

--- src/vitest.config.ts
+++ src/vitest.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
     test: {
         globals: true,
         environment: 'jsdom',
-        setupFiles: ['src/vitest.setup.ts'],
+        setupFiles: ['./vitest.setup.ts'],
         coverage: {
             provider: 'v8',
             reporter: ['text', 'json', 'html'],
@@ -23,7 +23,7 @@ export default defineConfig({
     },
     resolve: {
         alias: {
-            '@': path.resolve(__dirname, './src'),
+            '@': path.resolve(__dirname, '.'),
         },
     },
 });
